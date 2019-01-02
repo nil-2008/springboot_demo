1328eb0db10f3de116997b5488a96a1de52ac8ba
张连友 <ofzt@qq.com>
2019-01-02 12:07:18 +0800

    修改服务器和tomcat默认配置

--- Nil_database_jdbcTemplate/src/main/java/com/nil/database/bean/StudentsBean.java
+++ Nil_database_jdbcTemplate/src/main/java/com/nil/database/bean/StudentsBean.java
@@ -1,9 +1,14 @@
 package com.nil.database.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Date;
 
 /**
  * 学生实体类
@@ -17,7 +22,14 @@ import lombok.Setter;
 @Setter
 @Getter
 public class StudentsBean {
+  @JsonIgnore
   private int id;
   private String name;
+  /** 忽略 */
+  @JsonIgnore
   private int age;
+  /** 设置时间格式，或略空值 */
+  @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss a", locale = "zh", timezone = "Asia/shanghai")
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  private Date birthday;
 }
